4098048cf46fb9464b9e29e0ea144576418bd87d
Tanya Shylock <tourtanya@gmail.com>
2023-04-05 19:11:59 -0600

    edit for css and js

--- connect-four/connect4.js
+++ connect-four/connect4.js
@@ -18,7 +18,6 @@ const board = []; // array of rows, each row is array of cells  (board[y][x])
  */
 
 function makeBoard() {
-
   // TODO: set "board" to empty HEIGHT x WIDTH matrix array
   for (let i = 0; i < HEIGHT; i++) {
     let row = [];
@@ -34,7 +33,7 @@ function makeBoard() {
 /** makeHtmlBoard: make HTML table and row of column tops. */
 
 function makeHtmlBoard() {
-  const htmlBoard = document.getElementById('board');
+  const htmlBoard = document.getElementById("board");
 
   // TODO: add comment for this code
   // create a top cell of our column, set its attributes to
@@ -58,25 +57,22 @@ function makeHtmlBoard() {
   // uses WIDTH to create table cells for each row
   for (var y = 0; y < HEIGHT; y++) {
     // TODO: Create a table row element and assign to a "row" variable
-    let row = document.createElement('tr');
+    let row = document.createElement("tr");
 
     for (var x = 0; x < WIDTH; x++) {
       // TODO: Create a table cell element and assign to a "cell" variable
-    let cell = document.createElement('td');
-    cell.setAttribute('id', `c-${y}-${x}`);
+      let cell = document.createElement("td");
+      cell.setAttribute("id", `c-${y}-${x}`);
 
-    row.appendChild(cell);
-
+      row.appendChild(cell);
 
       // TODO: add an id, c-y-x, to the above table cell element
       // you'll use this later, so make sure you use c-y-x
 
       // TODO: append the table cell to the table row
-
     }
     // TODO: append the row to the html board
     htmlBoard.appendChild(row);
-
   }
 }
 
@@ -96,7 +92,9 @@ function findSpotForCol(x) {
 
 function placeInTable(y, x) {
   // TODO: make a div and insert into correct table cell
-  let piece = document.createElement('div');
+  let piece = document.createElement("div");
+  piece.classList.add("piece");
+  piece.classList.add(`player${currPLayer}`);
 
   let cell = document.getElementById(`c-${y}-${x}`);
   cell.appendChild(piece);
@@ -117,7 +115,7 @@ function handleClick(evt) {
   const x = Number(evt.target.id.slice("top-".length));
 
   // get next spot in column (if none, ignore click)
-  var y = findSpotForCol(x);
+  let y = findSpotForCol(x);
   if (y === null) {
     return;
   }
@@ -136,7 +134,7 @@ function handleClick(evt) {
   // TODO: check if all cells in board are filled; if so call, call endGame
   const isNotNull = 1;
   if (board[0].every(isNotNull)) {
-    endGame('Draw');
+    endGame("Draw");
   }
   // switch players
   // TODO: switch currPlayer 1 <-> 2
@@ -145,30 +143,32 @@ function handleClick(evt) {
 /** checkForWin: check board cell-by-cell for "does a win start here?" */
 
 function checkForWin() {
-
   /** _win:
    * takes input array of 4 cell coordinates [ [y, x], [y, x], [y, x], [y, x] ]
    * returns true if all are legal coordinates for a cell & all cells match
    * currPlayer
    */
   function _win(cells) {
-
     // TODO: Check four cells to see if they're all legal & all color of current
     // player
-
   }
 
   // using HEIGHT and WIDTH, generate "check list" of coordinates
   // for 4 cells (starting here) for each of the different
   // ways to win: horizontal, vertical, diagonalDR, diagonalDL
-  for (var y = 0; y < HEIGHT; y++) {
-    for (var x = 0; x < WIDTH; x++) {
+  for (let y = 0; y < HEIGHT; y++) {
+    for (let x = 0; x < WIDTH; x++) {
       // TODO: assign values to the below variables for each of the ways to win
       // horizontal has been assigned for you
       // each should be an array of 4 cell coordinates:
       // [ [y, x], [y, x], [y, x], [y, x] ]
 
-      let horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
+      let horiz = [
+        [y, x],
+        [y, x + 1],
+        [y, x + 2],
+        [y, x + 3],
+      ];
       let vert;
       let diagDL;
       let diagDR;
